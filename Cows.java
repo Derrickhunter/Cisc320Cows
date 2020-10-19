7e251f9231533fef02ee6e097b8a5605378a1b49
Derrickhunter <42981289+Derrickhunter@users.noreply.github.com>
2020-10-19 12:04:25 -0400

    Add files via upload

---
+++ Cows.java
@@ -0,0 +1,16 @@
+public class Cows{
+    /*
+    TODO:
+    
+    1) create strings values
+        latestWeight
+        lowestWeight
+        numOfMilks
+        totalMilk
+
+    2) toString Method
+    3) Maybe: compareTO
+    4) hashCode
+    
+     */
+}
