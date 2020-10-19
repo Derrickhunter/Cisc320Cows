9343031053d2cf50a693ea0ccd1d1d9d25e3d12e
Derrickhunter <42981289+Derrickhunter@users.noreply.github.com>
2020-10-19 12:04:48 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main{
+    
+     /*
+    TODO:
+    1) enter main method
+    2) enter file reader
+    3) enter arrayList
+    */
+}
